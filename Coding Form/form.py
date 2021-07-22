65fa36b7a87a14155d09361d038f928f857bfa5b
Sanjay <sanjykaria@gmail.com>
2021-07-22 18:06:56 +0530

    Additional Tabs

--- Coding Form/form.py
+++ Coding Form/form.py
@@ -7,6 +7,8 @@ def get_value():
     with open("file/entries", "a") as data:
         data.write("\n\nName: " + str(name.get()))
         data.write("\nAge: " + str(age.get()))
+        data.write("\nContact Number: " + str(con.get()))
+        data.write("\nGuardian Name: " + str(guardian.get()))
         data.write("\nSelected Language: " + str(language.get()))
     pass
 
@@ -21,19 +23,34 @@ enter.pack(pady=20)
 user = Label(root, text="Name")
 password = Label(root, text="Age")
 lang = Label(root, text="Language")
+contact = Label(root, text="Contact No.")
+guardian_name = Label(root, text="Guardian Name")
 
 name = StringVar()
 age = StringVar()
 language = StringVar()
+con = StringVar()
+guardian = StringVar()
 
 name_entry = Entry(root, textvariable=name)
 age_entry = Entry(root, textvariable=age)
+contact_entry = Entry(root, textvariable=con)
+guardian_entry = Entry(root, textvariable=guardian)
 language_entry = Entry(root, textvariable=language)
 
+# Name
 user.pack()
 name_entry.pack()
+# Age
 password.pack()
 age_entry.pack()
+# Contact
+contact.pack()
+contact_entry.pack()
+# Guardian Name
+guardian_name.pack()
+guardian_entry.pack()
+# Language
 lang.pack()
 language_entry.pack()
 

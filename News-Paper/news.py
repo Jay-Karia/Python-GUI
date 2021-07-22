7c3809fd543e0175058d3ecc3fe57ad215b42ecd
Sanjay <sanjykaria@gmail.com>
2021-07-22 17:52:02 +0530

    100% Done!

--- News-Paper/news.py
+++ News-Paper/news.py
@@ -6,17 +6,16 @@ import datetime
 root = Tk()
 
 root.geometry("1000x900")
-root.wm_minsize(780, 1500)
-root.wm_maxsize(11000, 11000)
+root.wm_minsize(1780, 1700)
+root.wm_maxsize(1700, 11000)
 root.title("NewsPaper")
 
-title = Label(text="Jay News", bg="black", fg="white", font="Consolas 25 bold")
-title.pack(fill=X)
+title = Label(text="Welcome To ", bg="black", fg="white", font="Consolas 25 bold", padx=50)
+title.grid(column=0)
 
-time = datetime.datetime.now()
+title = Label(text="Jay News", bg="black", fg="white", font="Consolas 25 bold", padx=50)
+title.grid(row=0, column=1, pady=5)
 
-date = Label(text=("Date: ", str(time)), bg="yellow", font="Consolas 15 bold")
-date.pack(pady=50, anchor="w")
 
 # Topic 1: Hacking
 
@@ -33,30 +32,11 @@ Info = Text(root, wrap=WORD, bg="orange", font="Consolas 12 bold", pady=20, padx
 Info.insert(INSERT, data)
 Info.config(height=12)
 
-hacking.pack(pady=50)
-Photo.pack()
-Info.pack(pady=30)
+hacking.grid(row=1, column=0, padx=50)
+Photo.grid(row=2, column=0)
+Info.grid(row=3, column=0)
 
-# Topic 2: Plants
-
-with open("files/plants.txt") as plant:
-    for p in plant:
-        data = p
-
-PImage = Image.open("images/plants.png")
-PPhoto = ImageTk.PhotoImage(PImage)
-
-plants = Label(text="Plants", font='Consolas 20 bold', bg="red", width=50)
-pPhoto = Label(image=PPhoto)
-PInfo = Text(root, wrap=WORD, bg="orange", font="Consolas 12 bold", pady=20, padx=10)
-PInfo.insert(INSERT, data)
-PInfo.config(height=8)
-
-plants.pack(pady=50)
-pPhoto.pack()
-PInfo.pack(pady=30)
-
-# Topic 3: Cricket
+# Topic 2: Cricket
 
 with open("files/cricket.txt") as crick:
     for c in crick:
@@ -67,15 +47,15 @@ CPhoto = ImageTk.PhotoImage(CImage)
 
 cricket = Label(text="Cricket", font='Consolas 20 bold', bg="red", width=50)
 cPhoto = Label(image=CPhoto)
-CInfo = Text(root, wrap=WORD, bg="orange", font="Consolas 12 bold", pady=50, padx=20)
+CInfo = Text(root, wrap=WORD, bg="orange", font="Consolas 12 bold", pady=20, padx=20)
 CInfo.insert(INSERT, data)
 CInfo.config(height=12)
 
-cricket.pack(pady=50)
-cPhoto.pack()
-CInfo.pack(pady=50)
+cricket.grid(row=1, column=1)
+cPhoto.grid(row=2, column=1)
+CInfo.grid(row=3, column=1)
 
-# Topic 4: Programming
+# Topic 3: Programming
 with open("files/programming.txt") as pro:
     for pr in pro:
         data = pr
@@ -85,28 +65,28 @@ PrPhoto = ImageTk.PhotoImage(PrImage)
 
 programming = Label(text="Programming", font='Consolas 20 bold', bg="red", width=50)
 pPhoto = Label(image=PrPhoto)
-PInfo = Text(root, wrap=WORD, bg="orange", font="Consolas 12 bold", pady=50, padx=20)
+PInfo = Text(root, wrap=WORD, bg="orange", font="Consolas 12 bold", pady=20, padx=20)
 PInfo.insert(INSERT, data)
-PInfo.config(height=10)
+PInfo.config(height=8)
 
-programming.pack(pady=50)
-pPhoto.pack()
-PInfo.pack(pady=50)
+programming.grid(row=4, column=0, pady=20)
+pPhoto.grid(row=5, column=0)
+PInfo.grid(row=6, column=0, pady=5)
 
-# Topic 5: Space
+# Topic 4: Space
 
 SImage = Image.open("images/space.png")
 SPhoto = ImageTk.PhotoImage(SImage)
 
 space = Label(text="Space", font='Consolas 20 bold', bg="red", width=50)
 sPhoto = Label(image=SPhoto)
-SInfo = Text(root, wrap=WORD, bg="orange", font="Consolas 12 bold", pady=50, padx=20)
+SInfo = Text(root, wrap=WORD, bg="orange", font="Consolas 12 bold", pady=20, padx=20)
 SInfo.insert(INSERT,
              '''The new findings match Andrea Dupree’s previous observations of Betelgeuse using the Hubble Space Telescope. Dupree, an astronomer at the Center for Astrophysics | Harvard & Smithsonian and a co-author on the new paper, captured signs of dense, heated material moving through the star’s atmosphere in the months leading up to the great dimming.“With Hubble, we could see the material as it left the star’s surface and moved out through the atmosphere, before the dust formed that caused the star to appear to dim,” Dupree says.Dupree found that the material moved about 200,000 miles per hour as it traveled from the star’s surface to its outer atmosphere. Once the gas bubble was millions of miles from the hot star, it cooled and formed a dust cloud that temporarily blocked the star’s light.The star returned to its normal brightness by April 2020.Dupree, who has been studying Betelgeuse since 1985, hopes to continue studying the star in hopes of catching it eject another gas bubble.''')
-SInfo.config(height=10)
+SInfo.config(height=8)
 
-space.pack(pady=50)
-sPhoto.pack()
-SInfo.pack(pady=50)
+space.grid(row=4, column=1, pady=20)
+sPhoto.grid(row=5, column=1)
+SInfo.grid(row=6, column=1, pady=5)
 
 root.mainloop()
